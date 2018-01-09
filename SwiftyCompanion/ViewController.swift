d245c0733f0db971f333426c58cabe0cea1f987d
Émilie LEGENT <alexandrelegent@gmail.com>
2018-01-09 14:33:01 +0100

    Initial Commit

---
+++ SwiftyCompanion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftyCompanion
+//
+//  Created by Émilie Legent on 09/01/2018.
+//  Copyright © 2018 Alexandre Legent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
