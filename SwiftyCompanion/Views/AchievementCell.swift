bc95d8c1d5d9ed4d0703ffdad25249928667f1d5
Émilie LEGENT <alexandrelegent@gmail.com>
2018-02-02 09:43:29 +0100

    Achievements are well displayed

--- SwiftyCompanion/Views/AchievementCell.swift
+++ SwiftyCompanion/Views/AchievementCell.swift
@@ -13,6 +13,20 @@ class AchievementCell: BaseCell {
     var achievement: Achievement? {
         didSet {
             guard let achievement = achievement else { return }
+            let attributed = NSMutableAttributedString(string: achievement.name, attributes: [
+                .font: UIFont.futuraBook(ofSize: 16),
+                .foregroundColor: UIColor.black
+            ])
+            
+            let attributedDescription = NSAttributedString(string: "\n\(achievement.description)", attributes: [
+                .font: UIFont.futuraBold(ofSize: 12),
+                .foregroundColor: UIColor.swiftyGray
+            ])
+            
+            attributed.append(attributedDescription)
+            textView.attributedText = attributed
+            
+            
             guard let url = URL(string: "https://api.intra.42.fr\(achievement.image)") else { return }
             let request = URLRequest(url: url)
             webView.loadRequest(request)
@@ -26,6 +40,14 @@ class AchievementCell: BaseCell {
         view.layer.borderWidth = 1
         view.layer.borderColor = UIColor.swiftyBorderGray.cgColor
         return view
+    }()
+    
+    private let textView: UITextView = {
+        let text = UITextView()
+        text.isScrollEnabled = false
+        text.isSelectable = false
+        text.isEditable = false
+        return text
     }()
     
     private let imageBackground: UIView = {
@@ -49,6 +71,7 @@ class AchievementCell: BaseCell {
     override func setupViews() {
         super.setupViews()
         addSubview(background)
+        addSubview(textView)
         background.addSubview(imageBackground)
         imageBackground.addSubview(webView)
     }
@@ -57,12 +80,16 @@ class AchievementCell: BaseCell {
         super.setupLayouts()
         _ = background.fill(self, constant: 10)
         
+        _ = textView.fill(.verticaly, background, constant: 2)
+        _ = textView.constraint(.leading, to: background, constant: 2)
+        _ = textView.constraint(.trailing, to: imageBackground, .leading, constant: 2)
+        
         _ = imageBackground.fill(.verticaly, background, constant: 1)
         _ = imageBackground.constraint(.trailing, to: background, constant: 1)
         _ = imageBackground.constraint(.width, to: background, multiplier: 0.3)
         
         _ = webView.center(imageBackground)
-        _ = webView.constraint(dimension: .width, constant: 70)
+        _ = webView.constraint(dimension: .width, constant: 50)
         _ = webView.constraint(.height, to: webView, .width)
     }
 }
